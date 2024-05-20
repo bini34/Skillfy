ba6e7eaac70de37c19948c8902455c5b95164e34
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-05-20 14:08:11 +0300

    add model

---
+++ Skillfy.Model/Catagorie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillfy.Model
+{
+    public class Catagorie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
