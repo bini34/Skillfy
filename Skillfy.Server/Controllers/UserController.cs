db3ffcac4b2a8bae93b75c83d0480f6b6e57f600
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-05-20 10:48:29 +0300

    add

---
+++ Skillfy.Server/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skillfy.Server.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
