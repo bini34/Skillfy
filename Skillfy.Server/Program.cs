cdf21c30baeea15a2bc45f02bfa6236818c00a36
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-05-31 17:32:12 +0300

    create new project

--- Skillfy.Server/Program.cs
+++ Skillfy.Server/Program.cs
@@ -1,24 +1,8 @@
-using Microsoft.EntityFrameworkCore;
-using Skillfy.DataAccess.Data;
-using Skillfy.Model;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options=> 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-         b => b.MigrationsAssembly("Skillfy.DataAccess"))); // Specify the migrations assembly
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-
 
 var app = builder.Build();
 
