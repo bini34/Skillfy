57c718ad5c895edb04da682712ad61ce8b36ffc8
dawitcode21 <106880663+dawitcode21@users.noreply.github.com>
2024-06-09 22:36:13 +0300

    videoid and uploadurl retriver complete

--- Skillfy.Server/Api/Chapacallback.cs
+++ Skillfy.Server/Api/Chapacallback.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skillfy.Server.Data;
+using Skillfy.Server.Model;
 
 namespace Skillfy.Server.Api
 {
@@ -6,11 +9,39 @@ namespace Skillfy.Server.Api
     [ApiController]
     public class Chapacallback
     {
+        public ApplicationDbContext _context;
 
 
-        
-        public Chapacallback() { 
-        
+        public Chapacallback(ApplicationDbContext context)
+        {
+            _context = context;
         }
+
+        public async Task EnrollUserAsync(string txRef, string customerEmail)
+        {
+            // Extract courseId and userId from txRef
+            var parts = txRef.Split('-');
+            var courseId = parts[0];
+            var userId = parts[1];
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId && u.Email == customerEmail);
+            var course = await _context.courses.FirstOrDefaultAsync(c => c.CourseID == courseId);
+
+            if (user != null && course != null)
+            {
+                var enrollment = new Enroll
+                {
+                    CourseID = course.Id,
+                    user = user.Id,
+                    EnrollmentDate = DateTime.UtcNow,
+                    PaymentStatus = "Paid"
+                };
+
+                _context.Enrollments.Add(enrollment);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
     }
 }
