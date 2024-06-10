b94e23e0587a0753a17f54b8988752ff87ab2340
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-06-10 14:59:01 +0300

    update autentication

--- skillfy.client/src/Services/authService.js
+++ skillfy.client/src/Services/authService.js
@@ -1,15 +1,9 @@
 // src/services/authService.js
 import axios from 'axios';
 
-const API_URL = 'https://localhost:7182/api/account'
+const API_URL = 'https://localhost:7182/api/account';
 
 const register = (FName, lName, Email, role, password) => {
-  console.log(FName)
-  console.log(lName)
-  console.log(Email)
-  console.log(role)
-  console.log(password)
-
   return axios.post(`${API_URL}/register`, {
     FName,
     lName,
@@ -27,35 +21,25 @@ const login = async (email, password) => {
     });
     if (response.data.accessToken) {
       localStorage.setItem('user', JSON.stringify(response.data));
-      console.log(response.data)
     }
     return response.data;
   } catch (error) {
     if (error.response) {
-      console.log(error.response.message)
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
       throw error.response.data;
     } else if (error.request) {
-      // The request was made but no response was received
-      console.log("Server did not respond. Please try again later.")
       throw new Error('Server did not respond. Please try again later.');
     } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("An unexpected error occurred.")
       throw new Error('An unexpected error occurred.');
-
     }
   }
 };
-
 
 const logout = () => {
   localStorage.removeItem('user');
 };
 
 const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem('userid'));
+  return JSON.parse(localStorage.getItem('user'));
 };
 
 const authService = {
