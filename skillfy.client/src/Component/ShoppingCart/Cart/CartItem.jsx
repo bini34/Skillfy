eff008e5bddaa9c9e133daac2a011273e255f0b8
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-06-21 17:13:29 +0300

    fix cart page

--- skillfy.client/src/Component/ShoppingCart/Cart/CartItem.jsx
+++ skillfy.client/src/Component/ShoppingCart/Cart/CartItem.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './CartItem.css';
-import DeleteIcon from '@mui/icons-material/Delete';
+// import DeleteIcon from '@mui/icons-material/Delete';
+import CancelPresentationIcon from '@mui/icons-material/CancelPresentation';
+import Avatar from '@mui/material/Avatar';
 
 const CartItem = ({ course }) => {
   return (
     <div className="cart-item">
-      <img src={course.image} alt={course.title} className="course-image" />
+      <img src={course.image} alt={course.title} className="cart-course-image" />
       <div className="course-info">
         <h3>{course.title}</h3>
-        <p>by {course.instructor}</p>
-        <div className="course-price">
-          <span className="discounted-price">${course.discountedPrice}</span>
+        <div className="cartCoueseInstructor">
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 24, height: 24 }} variant="square" />
+        <p> {course.instructor}</p>
         </div>
       </div>
-      <button className="remove-button"><DeleteIcon/></button>
+      <div className="course-price">
+          <span className="discounted-price">${course.discountedPrice}</span>
+      </div>
+      <button className="remove-button"><CancelPresentationIcon/></button>
     </div>
   );
 };
