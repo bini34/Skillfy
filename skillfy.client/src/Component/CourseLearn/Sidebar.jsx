3835477922a1ffa5ab334b54681462b32baaa361
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-06-26 10:01:22 +0300

    update

--- skillfy.client/src/Component/CourseLearn/Sidebar.jsx
+++ skillfy.client/src/Component/CourseLearn/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 import './Sidebar.css'
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-header">
+        <div className="sidebar-header-header">
         <Link to="/mycourse">
           <div className="circle">
             <KeyboardReturnIcon/>
@@ -13,9 +15,13 @@ const Sidebar = () => {
           Back
         </Link>
         <h1>Learn Adobe XD & Prototyping</h1>
+        </div>
+        <div className="progress">
         <p>60% Complete</p>
+        <BorderLinearProgress variant="determinate" value={50} />
+        </div>
       </div>
-
+      <input type="text" placeholder="Search" />
       <nav>
         <h3>Change simplification</h3>
         <ul>
