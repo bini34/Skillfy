7ecb3edd67889806a4e9e00e90b4bf82eae330f6
dawitcode21 <106880663+dawitcode21@users.noreply.github.com>
2024-07-11 12:19:17 +0300

    teacher payment updated

---
+++ Skillfy.Server/service/TeacherPaymentService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.EntityFrameworkCore;
+using Skillfy.Server.Data;
+
+
+namespace Skillfy.Server.service
+{
+    public class TeacherPaymentService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeacherPaymentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> payteacher(string bankno, int payment)
+        {
+           var account = await _context.banks.SingleOrDefaultAsync(b => b.BankAccount == bankno);
+            if (account != null)
+            {
+                account.balance += payment;
+                _context.SaveChanges();
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+        
+        }
+
+
+    }
+}
