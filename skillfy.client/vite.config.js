90b2526c7151fea2439e447b9c45b0df7885ed53
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-07-13 13:13:31 +0300

    Conditionally handle certificate creation based on environment

--- skillfy.client/vite.config.js
+++ skillfy.client/vite.config.js
@@ -6,39 +6,40 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder = env.APPDATA !== undefined && env.APPDATA !== ''
-    ? `${env.APPDATA}/ASP.NET/https`
-    : `${env.HOME}/.aspnet/https`;
+const isDevelopment = env.NODE_ENV === 'development';
+let httpsOptions = false;
 
-const certificateName = "skillfy.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+if (isDevelopment) {
+  const baseFolder = env.APPDATA !== undefined && env.APPDATA !== ''
+      ? `${env.APPDATA}/ASP.NET/https`
+      : `${env.HOME}/.aspnet/https`;
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit' }).status) {
-        throw new Error("Could not create certificate.");
-    }
+  const certificateName = "skillfy.client";
+  const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+  const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+  if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+      if (0 !== child_process.spawnSync('dotnet', [
+          'dev-certs',
+          'https',
+          '--export-path',
+          certFilePath,
+          '--format',
+          'Pem',
+          '--no-password',
+      ], { stdio: 'inherit' }).status) {
+          throw new Error("Could not create certificate.");
+      }
+  }
+
+  httpsOptions = {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath),
+  };
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7182';
-
-// Check if we're in a development environment
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-// Configure HTTPS options only if in development
-const httpsOptions = isDevelopment ? {
-    key: fs.readFileSync(keyFilePath),
-    cert: fs.readFileSync(certFilePath),
-} : false;
 
 // https://vitejs.dev/config/
 export default defineConfig({
